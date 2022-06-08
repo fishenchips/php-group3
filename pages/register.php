fe89bc4f81090e651c75509f047ccb447a2d8559
Anna M Attehed <anna.attehed@medieinstitutet.se>
2022-06-08 23:06:30 +0200

    minor changes

--- pages/register.php
+++ pages/register.php
@@ -9,7 +9,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/shop/assets/style.css">
+    <link rel="stylesheet" href="/php-group3/assets/style.css">
     <title>Register</title>
 
 </head>
