7735fb93915a98923cc9542056bd37a7a267ea19
Philip Carlsson <philipcarlsson@hotmail.com>
2022-06-15 22:02:05 +0200

    adding template functions to products.php

--- pages/admin-panel.php
+++ pages/admin-panel.php
@@ -2,6 +2,19 @@
 
 require_once __DIR__ . "/../classes/Template.php";
 
+$is_logged_in = isset($_SESSION["user"]);
+
+$logged_in_user = $is_logged_in ? $_SESSION["user"] : null;
+
+$is_admin = $is_logged_in && $logged_in_user->role == "admin";
+
+if (!$is_admin) {
+    http_response_code(401); //unauthorized
+?>
+    <a href="/php-group3">Go Back</a> <br>
+<?php
+    die("Access denied");
+}
 
 Template::header("Admin Panel", "");
 
